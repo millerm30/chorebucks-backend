ff27100191f9ba6ce576573805cfa95e49aef64d
Michael Miller <millergm30@gmail.com>
2022-10-02 09:37:09 -0230

    data

--- server.js
+++ server.js
@@ -1,14 +1,30 @@
 const express = require('express');
+const mysql = require('mysql2');
+const session = require('express-session');
 const app = express();
+require ('dotenv').config({ path: './.env' });
 const port = 8080;
 
-// setup the server main page for testing
+// Create database connection and use process.env to hide credentials
+const databaseConnection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
+
+// Connect to User Database
+databaseConnection.connect((err) => {
+  if (err) throw err;
+  console.log("Connected to the database!");
+});
+
+// Setup the server main page for testing
 app.get('/', (req, res) => {
   res.send("The server is running!");
 });
 
-// setup the server to listen on port 8080
+// Setup the server to listen on port 8080
 app.listen(port, () => {
   console.log(`Server is listening at http://localhost:${port}`);
 });
-
